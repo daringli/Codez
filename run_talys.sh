7a4731605690a78972c4906b819ccd22b4940131
Stefan Buller <bstefan@spare.fy.chalmers.se>
2015-06-11 14:46:47 +0200

    removed trash files

--- run_talys.sh
+++ run_talys.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-talys1-73 < talys_in > Z023N024E005j000.out
+talys1-73 < talys_in > Z090N130E020j000.out
--- scripts/plot_chart/plot_elem.m
+++ scripts/plot_chart/plot_elem.m
@@ -1,17 +1,20 @@
 function plot_elem(Z,N,decay,data)
   x=[N-0.5,N+0.5,N+0.5,N-0.5];
   y=[Z-0.5,Z-0.5,Z+0.5,Z+0.5];
-
-  if(strcmp("gamma", decay)==1)
-    color='g';
-  elseif(strcmp("p", decay)==1)
-    color='r';
-  elseif(strcmp("n", decay)==1)
-    color='b';
-  elseif(strcmp("alpha", decay)==1)
-    color='c';
+  if(isnan(data))
+    color='w'
   else
-    color='w';
+    if(strcmp("gamma", decay)==1)
+      color='g';
+    elseif(strcmp("p", decay)==1)
+      color='r';
+    elseif(strcmp("n", decay)==1)
+      color='b';
+    elseif(strcmp("alpha", decay)==1)
+      color='c';
+    else
+      color='w';
+    endif
   endif
     
   fill(x,y,color)
